c9b767c0f371f4c14b75c1d9fedd7cfabcc71a7f
WabisabiNeet <s037055@yahoo.co.jp>
2018-05-07 15:24:58 +0900

    プラグインI/F(やりかけ)

---
+++ salmonlog/SalmonlogNotifyPluginBase/SalmonlogNotifyPluginBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalmonlogNotifyPluginBase
+{
+    public interface SalmonlogNotifyPluginBase
+    {
+        /// <summary>
+        /// 通知：バイト開始
+        /// </summary>
+        void OnSalmonStarted();
+
+        /// <summary>
+        /// 通知：バイト終了(いる？)
+        /// </summary>
+        void OnSalmonFinished();
+
+        /// <summary>
+        /// 通知：ステージ名
+        /// </summary>
+        void NotifyStageName();
+
+        /// <summary>
+        /// 通知：バイト結果(全体概要)
+        /// </summary>
+        void NotifyResultSummary();
+
+        /// <summary>
+        /// 通知：プレイヤー個人成績
+        /// </summary>
+        void NotifyPersonalResult();
+
+        /// <summary>
+        /// 通知：やられた
+        /// </summary>
+        void NotifyDead();
+    }
+}
