2b785058330bdc81cf0768371221b252e3cd8ad0
WabisabiNeet <s037055@yahoo.co.jp>
2018-05-18 17:31:28 +0900

    commit(wip)

---
+++ salmonlog/SalmonlogCore/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace SalmonlogCore
+{
+    public class Context
+    {
+        #region public property
+        public Mat Frame { get; set; }
+
+        public DateTime FrameReadTime { get; set; }
+        #endregion
+
+        public Context()
+        {
+
+        }
+    }
+}
---
+++ salmonlog/SalmonlogCore/IScene.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalmonlogCore
+{
+    public abstract class Scene
+    {
+        protected Context ctx { get; set; }
+
+        public Scene(Context c)
+        {
+            ctx = c;
+        }
+
+        /// <summary>
+        /// 前回のマッチングから指定した時間以内にマッチングが実行されたかどうか
+        /// </summary>
+        /// <param name="duration_msec"></param>
+        /// <returns></returns>
+        protected bool MatchedIn(int duration_msec)
+        {
+            DateTime now = DateTime.Now;
+
+            var elapsed = now - ctx.FrameReadTime;
+            var elapsed_msec = elapsed.TotalMilliseconds;
+
+            return elapsed_msec < duration_msec;
+        }
+
+        #region abstract method.
+        public abstract bool Analyze();
+        #endregion
+    }
+}
